0f55907d9164683d26ca603eb65a23d07a035609
Katerina <katerina_kw@mail.ru>
2020-10-02 23:33:33 +0300

    Построение маршрутизации

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Main from "../main/main";
-
+import {Redirect, Switch, Route, BrowserRouter} from "react-router-dom";
+import SignIn from "../sign-in/sign-in";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
 
 const App = (props) => {
   const {rentalOfferCount} = props;
   return (
-    <Main
-      rentalOfferCount={rentalOfferCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main rentalOfferCount={rentalOfferCount}/>
+        </Route>
+        <Route exact path="/login">
+          <SignIn/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites/>
+        </Route>
+        <Route exact path="/offer/:id" component={Room}/>
+        <Redirect to="/"/>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+
+  return (
+    <React.Fragment>
+      <footer className="footer container">
+        <a className="footer__logo-link" href="main.html">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+        </a>
+      </footer>
+    </React.Fragment>
+  );
+};
+
+export default Footer;

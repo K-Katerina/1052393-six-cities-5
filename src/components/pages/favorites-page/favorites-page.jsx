e7a7584ed0a8b4d66aae61cd296ad190116ca935
Katerina <katerina_kw@mail.ru>
2020-10-12 11:22:10 +0300

    Вынесение компонентов из больших map

--- src/components/pages/favorites-page/favorites-page.jsx
+++ src/components/pages/favorites-page/favorites-page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Header from "../../header/header";
 import Footer from "../../footer/footer";
-import {getRating, groupOffersByLocation} from "../../../utils";
+import {groupOffersByLocation} from "../../../utils";
 import PropTypes from "prop-types";
 import {OfferPropType} from "../../../types";
 import {Link} from "react-router-dom";
+import FavoriteCard from "../../favorite-card/favorite-card";
 
 const FavoritesPage = (props) => {
   const {offers} = props;
@@ -29,39 +30,7 @@ const FavoritesPage = (props) => {
                     </div>
                     <div className="favorites__places">
                       {groupedOffers.get(location).map((offer) =>
-                        <article key={offer.id} className="favorites__card place-card">
-                          <div className="favorites__image-wrapper place-card__image-wrapper">
-                            <Link to={`/offer/${offer.id}`}>
-                              <img className="place-card__image" src={offer.preview} width="150" height="110"
-                                alt="Place image"/>
-                            </Link>
-                          </div>
-                          <div className="favorites__card-info place-card__info">
-                            <div className="place-card__price-wrapper">
-                              <div className="place-card__price">
-                                <b className="place-card__price-value">&euro;{offer.costPerNight}</b>
-                                <span className="place-card__price-text">&#47;&nbsp;night</span>
-                              </div>
-                              <button className={`${offer.isFavorite ? `place-card__bookmark-button--active` : ``} place-card__bookmark-button place-card__bookmark-button--active button`}
-                                type="button">
-                                <svg className="place-card__bookmark-icon" width="18" height="19">
-                                  <use xlinkHref="#icon-bookmark"></use>
-                                </svg>
-                                <span className="visually-hidden">In bookmarks</span>
-                              </button>
-                            </div>
-                            <div className="place-card__rating rating">
-                              <div className="place-card__stars rating__stars">
-                                <span style={{width: getRating(offer.rating)}}></span>
-                                <span className="visually-hidden">Rating</span>
-                              </div>
-                            </div>
-                            <h2 className="place-card__name">
-                              <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
-                            </h2>
-                            <p className="place-card__type">{offer.type}</p>
-                          </div>
-                        </article>
+                        <FavoriteCard key={offer.id} offer={offer}/>
                       )}
                     </div>
                   </li>
@@ -77,7 +46,7 @@ const FavoritesPage = (props) => {
 };
 
 FavoritesPage.propTypes = {
-  offers: PropTypes.arrayOf(OfferPropType),
+  offers: PropTypes.arrayOf(OfferPropType)
 };
 
 export default FavoritesPage;
--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -1,8 +1,8 @@
-import {getDate, getRating} from "../../utils";
 import React from "react";
 import PropTypes from "prop-types";
 import {ReviewPropType} from "../../types";
 import ReviewsForm from "../reviews-form/reviews-form";
+import ListReviews from "../list-reviews/list-reviews";
 
 const Reviews = (props) => {
   const {reviews} = props;
@@ -12,32 +12,7 @@ const Reviews = (props) => {
         <h2 className="reviews__title">Reviews &middot; <span
           className="reviews__amount">{reviews.length}</span></h2>
         <ul className="reviews__list">
-          {reviews.map((review) =>
-            <li key={review.id} className="reviews__item">
-              <div className="reviews__user user">
-                <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                  <img className="reviews__avatar user__avatar" src={review.author.avatar} width="54"
-                    height="54"
-                    alt="Reviews avatar"/>
-                </div>
-                <span className="reviews__user-name">
-                  {review.author.name}
-                </span>
-              </div>
-              <div className="reviews__info">
-                <div className="reviews__rating rating">
-                  <div className="reviews__stars rating__stars">
-                    <span style={{width: getRating(review.rating)}}></span>
-                    <span className="visually-hidden">Rating</span>
-                  </div>
-                </div>
-                <p className="reviews__text">
-                  {review.comment}
-                </p>
-                <time className="reviews__time" dateTime={review.date}>{getDate(review.date)}</time>
-              </div>
-            </li>
-          )}
+          <ListReviews reviews={reviews}/>
         </ul>
         <ReviewsForm/>
       </section>
